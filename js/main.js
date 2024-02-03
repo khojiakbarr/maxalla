5b549939e9e75c9f62a8b199d27dbd9c19839b93
khojiakbarr <chinozbekov09@gmail.com>
2024-02-03 20:37:24 +0500

    effect add

--- js/main.js
+++ js/main.js
@@ -18,3 +18,55 @@ tabletka.addEventListener("mouseout", (e) => {
 //? swiper
 
 
+var swiper = new Swiper(".mySwip__swiper", {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: ".swip-next",
+    prevEl: ".swip-prev",
+  },
+  breakpoints: {
+    // when window width is <= 767px
+    767: {
+      slidesPerView: 2,
+      spaceBetweenSlides: 40,
+    },
+    // when window width is <= 1300px
+    1300: {
+      slidesPerView: 2,
+      spaceBetweenSlides: 40,
+    },
+  },
+});
+
+var swiper = new Swiper(".prostata_swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
